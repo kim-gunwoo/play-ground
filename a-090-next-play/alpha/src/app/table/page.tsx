b68f5fbabfa34e4d1b58a680fe6ac6b4fdffd118
git-k <kgunu93@gmail.com>
2023-08-17 10:13:03 +0900

    table default

---
+++ a-090-next-play/alpha/src/app/table/page.tsx
@@ -0,0 +1,49 @@
+import Table, { TableCellType } from "@/components/table/Table"
+import { useMemo } from "react";
+
+interface IData {
+    name: string,
+    age: number,
+    addr: string
+}
+
+export default function TablePage() {
+    const collumns: TableCellType<IData> = useMemo(() => [
+        {
+            label: 'name', renderCell(data) {
+                return (
+                    <div style={{ width: '300px' }} >
+                        {data.name} <b> {`(${data.age})`}</b >
+                    </div >
+                )
+            }
+        },
+        { label: 'age', renderCell: (data) => data.age },
+        { label: 'addr', renderCell: (data) => data.addr },
+        {
+            label: 'dom',
+            renderLabel(label) {
+                return <div>
+                    {label}<b>!!</b>
+                </div>
+            },
+            renderCell: () => ''
+        },
+    ], []);
+    const datas = [
+        { name: 'ap1', age: 10, addr: 'seoul' },
+        { name: 'ap2', age: 120, addr: 'seoul-1' },
+        { name: 'ap3', age: 30, addr: 'seoul' },
+        { name: 'ap4', age: 110, addr: 'seoul-1' },
+    ]
+
+    return (
+        <div>
+            <Table
+                collumns={collumns}
+                datas={datas}
+            />
+            <br />
+        </div>
+    )
+}
---
+++ a-090-next-play/alpha/src/components/table/Table.tsx
@@ -0,0 +1,62 @@
+interface IHeader<T> {
+    collumns: Omit<TableCell<T>, 'renderCell'>[],
+}
+
+export function Header<T>({ collumns }: IHeader<T>) {
+    return (
+        <thead >
+            <tr>
+                {collumns.map((collumn, idx) => (
+                    <th key={`collumns-${collumn.label}-${idx}`}>
+                        {collumn.renderLabel ? collumn.renderLabel(collumn.label) : collumn.label}
+                    </th>
+                ))}
+            </tr >
+        </thead>
+    )
+}
+
+interface IBody<T> {
+    collumns: Pick<TableCell<T>, 'renderCell' | 'label'>[],
+    datas: T[]
+}
+
+export function Body<T>({ collumns, datas }: IBody<T>) {
+    return (
+        <tbody>
+            {datas.map((row, idx) => (
+                <tr key={`body-data-row-${idx}`}>
+                    {collumns.map((col, idx) =>
+                        <td key={`col-${col.label}-${idx}`}>{col.renderCell(row)}</td>
+                    )}
+                </tr >
+            ))}
+        </tbody>
+    )
+}
+
+
+export type TableCell<T> = {
+    label: string,
+    renderCell: (data: T) => React.ReactNode
+    renderLabel?: (label: string) => React.ReactNode
+}
+export type TableCellType<T> = TableCell<T>[]
+
+
+interface ITable<T> {
+    collumns: TableCellType<T>,
+    datas: T[]
+}
+
+export default function Table<T>({ collumns, datas }: ITable<T>) {
+    return (
+        <table>
+            <Header collumns={collumns} />
+            <Body
+                collumns={collumns}
+                datas={datas}
+            />
+        </table >
+    )
+}
