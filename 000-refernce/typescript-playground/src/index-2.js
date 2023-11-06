cc2690de971aac8d684192658354ebb80904332a
git-k <kgunu93@gmail.com>
2023-11-06 21:05:20 +0900

    add typescript play ground

---
+++ 000-refernce/typescript-playground/src/index-2.js
@@ -0,0 +1,50 @@
+"use strict";
+// namespace
+// import * as shapes from './shapes';
+// import { Shapes } from './shapes';
+// const t = new Shapes.Triangle(); // shapes.Shapes?
+Object.defineProperty(exports, "__esModule", { value: true });
+// export class Triangle {
+//   /* ... */
+// }
+// export class Square {
+//   /* ... */
+// }
+var Triangle = /** @class */ (function () {
+    function Triangle() {
+    }
+    return Triangle;
+}());
+var Square = /** @class */ (function () {
+    function Square() {
+    }
+    return Square;
+}());
+var shapes = {
+    Triangle: Triangle,
+    Square: Square,
+};
+exports.default = shapes;
+// eslint-disable-next-line @typescript-eslint/no-namespace
+// export namespace Shapes {
+//   export class Triangle {
+//     /* ... */
+//   }
+//   export class Square {
+//     /* ... */
+//   }
+// }
+// const Utils = {
+//   add(a: number, b: number) {
+//     return a + b;
+//   },
+// };
+// module
+// import * as shapes from './shapes';
+// let t = new shapes.Triangle();
+// import * as shapes from './shapes';
+// const t = new shapes.Triangle();
+// import shapes from './shapes';
+// const t = new shapes.Triangle();
+// console.log(t);
+// Utils.add(1, 2);
---
+++ 000-refernce/typescript-playground/src/test-2.js
@@ -0,0 +1,4 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var _1 = require(".");
+new _1.default.Square();
